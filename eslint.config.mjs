e5c0e8c67d1c7532f8b2803244c3f2335fab32d7
Waqas Shaukat <waqas.lhr.pnjb@gmail.com>
2025-12-02 22:14:31 +0500

    modification in github actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,10 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "coverage/**",
+      "test-results/**",
+      "jest.config.js",
+      "jest.setup.js",
     ],
   },
 ];
