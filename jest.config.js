9c2a34e7a47f0b5fab45407206eccee35f65f4c7
Waqas Shaukat <waqas.lhr.pnjb@gmail.com>
2025-12-02 22:33:04 +0500

    test files path fix

--- jest.config.js
+++ jest.config.js
@@ -37,7 +37,7 @@ const customJestConfig = {
       'jest-junit',
       {
         outputDirectory: './test-results',
-        outputName: 'junit.xml',
+        outputName: process.env.JEST_JUNIT_OUTPUT_NAME || 'junit.xml',
         ancestorSeparator: ' › ',
         uniqueOutputName: 'false',
         suiteNameTemplate: '{filepath}',
